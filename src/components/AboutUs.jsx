970754488b154cc854da7df84d696ba0c79093a4
Parthasarathi Poola <poolaparthasarathi@gmail.com>
2026-05-06 14:38:59 +0530

    Update Usha image source to ProfilePic.jpeg

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import UshaImg from '../img/Usha';
+import UshaImg from '../img/ProfilePic.jpeg';
 import NatashaImg from '../img/Natasha.webp';
 import PardhuImg from '../img/ProfilePic.jpeg';
 import NavyaImg from '../img/Navya.webp';
